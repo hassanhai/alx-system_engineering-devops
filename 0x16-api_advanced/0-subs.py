5537cb4062ee211bd5865473ec53aaed9fcf3927
hassanhai <hassan.mouilha@gmail.com>
2024-01-16 14:44:16 -0800

    up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"subscribers count"
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    headers = {
-        'User-Agent': 'My User Agent 1.0',
-    }
-    params = {
-        'format': 'json',
-    }
-    response = requests.get('https://oauth.reddit.com/r/' + subreddit + '/about', headers=headers, params=params)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
